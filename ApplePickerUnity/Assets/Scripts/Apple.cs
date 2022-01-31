633490be3dddd6216451ac426c5b19c6288cd22a
garrondenney74 <56270816+garrondenney74@users.noreply.github.com>
2022-01-31 16:41:35 -0600

    Update on ApplePicker

---
+++ ApplePickerUnity/Assets/Scripts/Apple.cs
@@ -0,0 +1,40 @@
+/***
+ * Created By: Garron Denney
+ * Date Created: 1/31/22
+ * 
+ * Last Edited: N/A
+ * Last Edited By: N/A
+ * 
+ * Description: Apple despawn logic
+ */
+
+
+
+
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
